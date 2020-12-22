7433cfba0d1b79e476acdce46db5dd889454ae50
Adriano <dev@dude333.com>
2020-12-22 17:29:27 -0300

    Improve def

--- myspider.py
+++ myspider.py
@@ -11,6 +11,8 @@ class BlogSpider(scrapy.Spider):
     def parse(self, response):
         for row in response.xpath("//tr"):
             line = row.css("td *::text").extract()
+            valFromTitle(line, "Competência")
+            valFromTitle(line, "Código ISIN")
             # clean = [l.encode("utf-8").strip() for l in line]
             # print(clean[0].decode('ascii') + ": " + clean[1].decode('ascii'))
             # print(u' '.join(clean).encode('utf-8').strip())
@@ -19,19 +21,15 @@ class BlogSpider(scrapy.Spider):
             #     print(f"{line[0].strip()}\t|\t{line[1].strip()}")
             # else:
             #     print(f"{line[0].strip()}")
-            if len(line) >= 2 and "Competência" in line[0]:
-                print(f"{line[0].strip()}\t|\t{line[1].strip()}")
-            valFromTitle(line, "Competência", 0, 1)
 
 
-def valFromTitle(row, title_str, title_col, val_col):
+def valFromTitle(row, title):
     """
     title_str: title string
-    title_col: position from title on row array
-    val_col:   position from value on row array
     """
-    if len(row) >= max(title_col, val_col) and title_str in row[title_col]:
-        print(f"{row[title_col].strip()}\t|\t{row[val_col].strip()}")
+    for i, val in enumerate(row):
+        if title in val and i + 1 < len(row):
+            print(f"{row[i].strip()}\t|\t{row[i+1].strip()}")
 
 
 # Competência
