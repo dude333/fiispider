b4946c47e9002687bd83421bdd547cd63d797a05
Adriano <dev@dude333.com>
2020-12-23 09:40:59 -0300

    Print report

--- myspider.py
+++ myspider.py
@@ -5,46 +5,78 @@ import locale
 class BlogSpider(scrapy.Spider):
     name = "blogspider"
     start_urls = [
-        "https://fnet.bmfbovespa.com.br/fnet/publico/exibirDocumento?id=131924"
+        "https://fnet.bmfbovespa.com.br/fnet/publico/exibirDocumento?id=131924",
+        "https://fnet.bmfbovespa.com.br/fnet/publico/exibirDocumento?id=127183",
+        "https://fnet.bmfbovespa.com.br/fnet/publico/exibirDocumento?id=133113",
     ]
 
     def parse(self, response):
+        rendimentos = 0
+        valor_a_receber = 0
+        num_cotas = 0
+        taxa_adm = 0
+        taxa_perf = 0
+
+        print("\n-----\n")
+
         for row in response.xpath("//tr"):
             line = row.css("td *::text").extract()
+
+            valFromTitle(line, "Nome do Fundo")
+            valFromTitle(line, "CNPJ do Fundo")
             valFromTitle(line, "Competência")
             valFromTitle(line, "Código ISIN")
-            valFromTitle(line, "Número de Cotas Emitidas")
+            valFromTitle(line, "Número de cotistas")
+            if num_cotas == 0:
+                num_cotas = valFromTitle(line, "Número de Cotas Emitidas")
             valFromTitle(line, "Ativo – R$")
             valFromTitle(line, "Total investido")
             valFromTitle(line, "Direitos reais sobre bens imóveis")
-            valFromTitle(line, "Valores a Receber")
+            if valor_a_receber == 0:
+                valor_a_receber = valFromTitle(line, "Valores a Receber")
             valFromTitle(line, "Contas a Receber por Aluguéis")
             valFromTitle(line, "Total do passivo")
             valFromTitle(line, "Patrimônio Líquido – R$")
-            valFromTitle(line, "Rendimentos a distribuir")
-            valFromTitle(line, "Taxa de administração a pagar")
-            valFromTitle(line, "Taxa de performance a pagar")
+            if rendimentos == 0:
+                rendimentos = valFromTitle(line, "Rendimentos a distribuir")
+            if taxa_adm == 0:
+                taxa_adm = valFromTitle(line, "Taxa de administração a pagar")
+            if taxa_perf == 0:
+                taxa_perf = valFromTitle(line, "Taxa de performance a pagar")
+
+        if num_cotas != 0:
+            print(f"{'Aluguéis/Cota':30.30s} | {valor_a_receber/num_cotas:0.3f}")
+            print(f"{'Rendimentos/Cota':30.30s} | {rendimentos/num_cotas:0.3f}")
+            print(f"{'Taxas/Cota':30.30s} | {(taxa_adm+taxa_perf)/ num_cotas:0.3f}")
+
+        if rendimentos != 0:
+            print(
+                f"{'Taxas/Rendimentos':30.30s} | {100*(taxa_adm+taxa_perf)/ rendimentos:0.3f}%"
+            )
 
 
 def valFromTitle(row, title):
     """
     title_str: title string
     """
-    # locale.setlocale(locale.LC_ALL, "pt_BR.UTF8")
+    # locale.setlocale(locale.LC_ALL, "Portuguese_Brazil.1252")
+    locale.setlocale(locale.LC_ALL, "pt_BR.UTF-8")
     for i, val in enumerate(row):
         if title == "Ativo – R$" and title in val and i + 3 < len(row):
             print(f"{row[i].strip():30.30s} | {row[i+3].strip()}")
             try:
-                return locale.atoi(row[i + 3].strip())
-            except ValueError:
-                return 0
+                return locale.atof(row[i + 3].strip())
+            except Exception:
+                return 0.0
 
         if title in val and i + 1 < len(row):
             print(f"{row[i].strip():30.30s} | {row[i+1].strip()}")
             try:
-                return locale.atoi(row[i + 1].strip())
-            except ValueError:
-                return 0
+                return locale.atof(row[i + 1].strip())
+            except Exception:
+                return 0.0
+
+    return 0.0
 
 
 # Competência
